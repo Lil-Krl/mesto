867271788cc8d821d19010ee4f997ab8a5db1e53
Lil-Krl <Lil-Krl@yandex.ru>
2022-10-09 17:27:23 +0300

    Работа над js

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,47 @@
 let editProfileIcon = document.querySelector('.profile__edit');
 
-let editProfilePopup = document.querySelector('.popup-edit');
+let addCardIcon = document.querySelector('profile__add-btn');
 
-let closeIconPopup = editProfilePopup.querySelector('.popup-edit__close');
+let editProfilePopup = document.querySelector('#popup-profile');
 
-let name = document.querySelector('.profile__name');
+let closeIconPopup = editProfilePopup.querySelector('.popup__close');
 
-let activity = document.querySelector('.profile__activity');
+let cardAddPopup = document.querySelector('#popup-cards');
 
-let nameInput = editProfilePopup.querySelector('.popup-edit__input-item_type_name');
+let imgZoomPopup = document.querySelector('#image-popup');
 
-let activityInput = editProfilePopup.querySelector('.popup-edit__input-item_type_activity');
+let imgZoomPopupDescription = imgZoomPopup.querySelector('.popup__description');
+
+let imgZoomPopupImage = imgZoomPopup.querySelector('.popup__image');
+
+let profileName = document.querySelector('.profile__name');
+
+let profileActivity = document.querySelector('.profile__activity');
+
+let nameInput = editProfilePopup.querySelector('.popup__input-item_type_name');
+
+let activityInput = editProfilePopup.querySelector('.popup__input-item_type_activity');
+
+let cardNameInput = cardAddPopup.querySelector('.popup__input-item_type_name');
+
+let cardLinkInput = cardAddPopup.querySelector('.popup__input-item_type_activity');
+
+let cardsArea = document.querySelector('.cards');
 
 function popupOpen() {
-  editProfilePopup.classList.add('popup-edit_opened');
-  nameInput.value = name.textContent;
-  activityInput.value = activity.textContent;
+  editProfilePopup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  activityInput.value = profileActivity.textContent;
 }
 
 function popupClose() {
-  editProfilePopup.classList.remove('popup-edit_opened');
+  editProfilePopup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  name.textContent = nameInput.value;
-  activity.textContent = activityInput.value;
+  profileName.textContent = nameInput.value;
+  profileActivity.textContent = activityInput.value;
   popupClose();
 }
 
