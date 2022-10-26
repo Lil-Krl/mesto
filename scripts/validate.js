2922f3ee17e565461273b751475e5aa222802ab7
Lil-Krl <Lil-Krl@yandex.ru>
2022-10-27 01:56:02 +0300

    almost done

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,73 +1,69 @@
-const MIN_LENGTH = 2
-const MAX_LENGTH = 40
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
+  inputElement.classList.add('popup__input-item_type_error')
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-item_type_visible')
+};
 
-function showError(form, errorSelector, i, text) {
-  const labels = form.querySelectorAll(errorSelector);
-  for (let j = 0; j < labels.length; ++j) {
-    if (labels[j].getAttribute('data-number') == i + 1) {
-      labels[j].innerHTML = text;
-    }
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
+  inputElement.classList.remove('popup__input-item_type_error')
+  errorElement.classList.remove('popup__input-item_type_visible')
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
   }
-}
-
-
-function validate(form, errorSelector, inputs, inputErrorClass) {
-  let answer = true;
-  for (let i = 0; i < inputs.length; ++i) {
-    const value = inputs[i].value
-    if (inputs[i].value == '') {
-      inputs[i].classList.add(inputErrorClass);
-      showError(form, errorSelector, i, "Вы пропустили это поле")
-    } else if (inputs[i].classList.contains("url")) {
-      if (! /^(http|https):\/\/[^ "]+$/.test(inputs[i].value) ) {
-        const text = 'Введите адрес сайта';
-        showError(form, errorSelector, i, text)
-        inputs[i].classList.add(inputErrorClass);
-      } else {
-        showError(form, errorSelector, i, "")
-        inputs[i].classList.remove(inputErrorClass);
-      }
-    } else if (value.length < MIN_LENGTH || value.length > MAX_LENGTH) {
-      inputs[i].classList.add(inputErrorClass);
-      const text = 'Минимальное количество символов: ' + MIN_LENGTH + '. Длина текста сейчас: ' + value.length + ' символ.'
-      showError(form, errorSelector, i, text)
-      answer = false;
-    } else {
-      showError(form, errorSelector, i, '')
-      inputs[i].classList.remove(inputErrorClass);
-    }
+  else {
+    hideInputError(formElement, inputElement);
   }
-  return answer
-}
+};
 
-function enableValidation(settings) {
-  const forms = document.querySelectorAll(settings.formSelector);
-  for (let i = 0; i < forms.length; ++i) {
-    const inputs = forms[i].querySelectorAll(settings.inputSelector);
-    const submitButton = forms[i].querySelector(settings.submitButtonSelector);
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input-item'));
+  const buttonElement = formElement.querySelector('.popup__submit');
 
-    for (let j = 0; j < inputs.length; ++j) {
-      inputs[j].oninput = (event) => {
-        const validation = validate(forms[i], settings.errorClass, inputs, settings.inputErrorClass)
-        if (!validation) {
-          submitButton.disabled = true
-          submitButton.classList.add('popup__submit_disabled')
-        } else {
-          submitButton.disabled = false
-          submitButton.classList.remove('popup__submit_disabled')
-        }
-      }
-    }
-  }
-}
+  toggleButtonState(inputList, buttonElement);
 
-window.onload = function() {
-  enableValidation({
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input-item',
-    submitButtonSelector: '.popup__submit',
-    inactiveButtonClass: '.popup__submit_disabled',
-    inputErrorClass: '.popup__input-item_type_error',
-    errorClass: '.popup__error-visible'
+
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
   });
+}
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+
+    const fieldsetList = Array.from(formElement.querySelectorAll('.popup__form-set'));
+    
+    fieldsetList.forEach((fieldSet) => {
+  setEventListeners(fieldSet);
+  });
+});
+};
+
+enableValidation();
+
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}
+
+function toggleButtonState (inputList, buttonElement) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__submit_disabled');
+  } else {
+    buttonElement.classList.remove('popup__submit_disabled');
+  }
 }
